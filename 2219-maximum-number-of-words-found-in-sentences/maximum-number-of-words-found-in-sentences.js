b06a89430b978a052ff1a3245e5e191dae941fec
Apurba Adhikary⚡ <apurba.apurbaadhikary@gmail.com>
2024-03-04 21:34:47 +0530

    Time: 59 ms (41.74%) | Memory: 50.8 MB (55.71%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function(sentences) {
+    let max = 0
+    for(let i = 0; i < sentences.length; i++){
+        let count = sentences[i].trim().split(' ')
+        if(max < count.length){
+            max = count.length
+        }
+    }
+    return max
+};
