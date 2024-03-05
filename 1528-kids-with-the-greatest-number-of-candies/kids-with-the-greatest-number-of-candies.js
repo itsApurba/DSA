c6c11fa4dc1aab70be61585b5de724627110acf7
Apurba Adhikary⚡ <apurba.apurbaadhikary@gmail.com>
2024-03-05 15:15:19 +0530

    Time: 55 ms (69.72%) | Memory: 50.8 MB (12.54%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = Math.max(...candies)
+    const res = []
+    for(let i = 0; i < candies.length; i++){
+        if(candies[i]+extraCandies >= max){
+            res.push(true)
+        }else{
+            res.push(false)
+        }
+    }
+    return res
+};
